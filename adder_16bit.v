eda3e811b66ab5d1126c7aadbeff807b98c69217
anshtyagi04 <ansht2889@gmail.com>
2026-06-29 17:37:02 +0530

    Create adder_16bit.v

---
+++ adder_16bit.v
@@ -0,0 +1,13 @@
+module adder_16bit(
+    input [15:0] x,y,
+    output [15:0] z,
+    output sign,zero,carry,parity,overflow
+);
+
+assign {carry,z}=x+y;
+assign sign=z[15];
+assign zero = ~|z;
+assign parity= ^~ z;
+assign overflow= (x[15]&y[15]& ~z[15]) | (~x[15]& ~y[15]&z[15]);
+
+endmodule
